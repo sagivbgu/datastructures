e5314820ddf48325e017e25363984f7682531a9f
sagivsc <sagivsc@post.bgu.ac.il>
2019-04-03 01:21:22 +0300

    Reformat (prettify) code

--- FloorsArrayList.java
+++ FloorsArrayList.java
@@ -3,17 +3,17 @@ public class FloorsArrayList implements DynamicSet {
     private FloorsArrayLink negtiveInfintyLink;
     private int size;
     private int maxArrSize;
-    
-    
-    public FloorsArrayList(int N){
-       this.infintyLink=new FloorsArrayLink(Double.POSITIVE_INFINITY, N);
-       this.negtiveInfintyLink=new FloorsArrayLink(Double.NEGATIVE_INFINITY, N);
-       this.size=0;
-       this.maxArrSize=0;
+
+
+    public FloorsArrayList(int N) {
+        this.infintyLink = new FloorsArrayLink(Double.POSITIVE_INFINITY, N);
+        this.negtiveInfintyLink = new FloorsArrayLink(Double.NEGATIVE_INFINITY, N);
+        this.size = 0;
+        this.maxArrSize = 0;
     }
 
     @Override
-    public int getSize(){
+    public int getSize() {
         //@TODO: implement
         return 0;
     }
@@ -30,44 +30,38 @@ public class FloorsArrayList implements DynamicSet {
 
     @Override
     public FloorsArrayLink lookup(double key) {
-    	FloorsArrayLink prevLink=null;
-    	FloorsArrayLink currLink=this.negtiveInfintyLink;
-    	FloorsArrayLink nextLink= this.negtiveInfintyLink.getNext(this.maxArrSize-1);
-        int i=this.maxArrSize-1;
-        while(nextLink!=prevLink || i!=0)
-        {
-        	if(key==currLink.getKey())
-        		return currLink;
+        FloorsArrayLink prevLink = null;
+        FloorsArrayLink currLink = this.negtiveInfintyLink;
+        FloorsArrayLink nextLink = this.negtiveInfintyLink.getNext(this.maxArrSize - 1);
+        int i = this.maxArrSize - 1;
+        while (nextLink != prevLink || i != 0) {
+            if (key == currLink.getKey())
+                return currLink;
 
-           	if(key>currLink.getKey() && key<nextLink.getKey())
-        	{
-           		if(i>0)
-           			i--;
-        		prevLink=currLink;
-        		currLink=nextLink;
-        		nextLink=nextLink.getNext(i);
-        		
-        	}
-        	else if(key<currLink.getKey() && key>nextLink.getKey())
-        	{
-           		if(i>0)
-           			i--;
-        		prevLink=currLink;
-        		currLink=nextLink;
-        		nextLink=nextLink.getPrev(i);	
-        	}
-        	else if(key>currLink.getKey() && key>nextLink.getKey())
-        	{
-        		prevLink=currLink;
-        		currLink=nextLink;
-        		nextLink=nextLink.getNext(i);
-        	}
-        	else if(key<currLink.getKey() && key<nextLink.getKey())
-        	{
-        		prevLink=currLink;
-        		currLink=nextLink;
-        		nextLink=nextLink.getPrev(i);
-        	}
+            if (key > currLink.getKey() && key < nextLink.getKey()) {
+                if (i > 0)
+                    i--;
+                prevLink = currLink;
+                currLink = nextLink;
+                nextLink = nextLink.getNext(i);
+
+            } else if (key < currLink.getKey() && key > nextLink.getKey()) {
+                if (i > 0)
+                    i--;
+                prevLink = currLink;
+                currLink = nextLink;
+                nextLink = nextLink.getPrev(i);
+
+            } else if (key > currLink.getKey() && key > nextLink.getKey()) {
+                prevLink = currLink;
+                currLink = nextLink;
+                nextLink = nextLink.getNext(i);
+
+            } else if (key < currLink.getKey() && key < nextLink.getKey()) {
+                prevLink = currLink;
+                currLink = nextLink;
+                nextLink = nextLink.getPrev(i);
+            }
         }
         return null;
     }
@@ -95,9 +89,8 @@ public class FloorsArrayList implements DynamicSet {
         //@TODO: implement
         return 0;
     }
-    
-    private boolean isBetween(double curent,double next,double key)
-    {
-    	return () || (key<curent && key>next);
+
+    private boolean isBetween(double curent, double next, double key) {
+        return () || (key < curent && key > next);
     }
 }
