91bf15ca4285599b3c30cf3667a27b73750fa8ca
michael-simonovsky <38807218+michael-simonovsky@users.noreply.github.com>
2019-04-02 16:31:59 +0300

    Add files via upload

---
+++ FloorsArrayLink.java
@@ -0,0 +1,36 @@
+public class FloorsArrayLink {
+    //@TODO: add fields
+
+    public FloorsArrayLink(double key, int arrSize){
+        //@TODO: implement
+    }
+
+    public double getKey() {
+        //@TODO: implement
+        return 0;
+    }
+
+    public FloorsArrayLink getNext(int i) {
+        //@TODO: implement
+        return null;
+    }
+
+    public FloorsArrayLink getPrev(int i) {
+        //@TODO: implement
+        return null;
+    }
+
+    public void setNext(int i, FloorsArrayLink next) {
+        //@TODO: implement
+    }
+
+    public void setPrev(int i, FloorsArrayLink prev) {
+        //@TODO: implement
+    }
+
+    public int getArrSize(){
+        //@TODO: implement
+        return 0;
+    }
+}
+
---
+++ FloorsArrayList.java
@@ -0,0 +1,53 @@
+public class FloorsArrayList implements DynamicSet {
+    //@TODO: add fields
+
+    public FloorsArrayList(int N){
+        //@TODO: implement
+    }
+
+    @Override
+    public int getSize(){
+        //@TODO: implement
+        return 0;
+    }
+
+    @Override
+    public void insert(double key, int arrSize) {
+        //@TODO: implement
+    }
+
+    @Override
+    public void remove(FloorsArrayLink toRemove) {
+        //@TODO: implement
+    }
+
+    @Override
+    public FloorsArrayLink lookup(double key) {
+        //@TODO: implement
+        return null;
+    }
+
+    @Override
+    public double successor(FloorsArrayLink link) {
+        //@TODO: implement
+        return 0;
+    }
+
+    @Override
+    public double predecessor(FloorsArrayLink link) {
+        //@TODO: implement
+        return 0;
+    }
+
+    @Override
+    public double minimum() {
+        //@TODO: implement
+        return 0;
+    }
+
+    @Override
+    public double maximum() {
+        //@TODO: implement
+        return 0;
+    }
+}
