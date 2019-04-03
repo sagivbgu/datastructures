41fa23368f718615f3dc0729e9ced59319c0dcb8
sagivsc <sagivsc@post.bgu.ac.il>
2019-04-03 20:28:18 +0300

    Reformat (prettify) FloorsArrayLink

--- src/FloorsArrayLink.java
+++ src/FloorsArrayLink.java
@@ -1,16 +1,16 @@
 public class FloorsArrayLink {
-    
-    private	double key;
+
+    private double key;
     private final int arrSize;
     private FloorsArrayLink[] arrFront;
     private FloorsArrayLink[] arrBack;
-	
-    public FloorsArrayLink(double key, int arrSize){
-       this.key=key;
-       this.arrSize=arrSize;
-       this.arrBack= new FloorsArrayLink[arrSize];
-       this.arrFront= new FloorsArrayLink[arrSize];
-       
+
+    public FloorsArrayLink(double key, int arrSize) {
+        this.key = key;
+        this.arrSize = arrSize;
+        this.arrBack = new FloorsArrayLink[arrSize];
+        this.arrFront = new FloorsArrayLink[arrSize];
+
     }
 
     public double getKey() {
@@ -18,36 +18,34 @@ public class FloorsArrayLink {
     }
 
     public FloorsArrayLink getNext(int i) {
-        if(this.arrSize<i)
-        	return null;
-        return this.arrFront[i-1];
+        if (this.arrSize < i)
+            return null;
+        return this.arrFront[i - 1];
     }
 
     public FloorsArrayLink getPrev(int i) {
-        if(this.arrSize<i)
-        	return null;
-        return this.arrBack[i-1];
+        if (this.arrSize < i)
+            return null;
+        return this.arrBack[i - 1];
     }
 
     public void setNext(int i, FloorsArrayLink next) {
-        if(this.arrSize>=i)
-        {
-        	this.arrFront[i-1]=next;
+        if (this.arrSize >= i) {
+            this.arrFront[i - 1] = next;
         }
 
     }
 
     public void setPrev(int i, FloorsArrayLink prev) {
-        if(this.arrSize>=i)
-        {
-        	this.arrBack[i-1]=prev;
+        if (this.arrSize >= i) {
+            this.arrBack[i - 1] = prev;
         }
     }
 
-    public int getArrSize(){
-         return arrSize;
+    public int getArrSize() {
+        return arrSize;
     }
-    
-    
+
+
 }
 
